70f63616e77a37ad61a77b6e0b85dc43dee4cfa9
davidbesonen <42583281+davidbesonen@users.noreply.github.com>
2018-09-14 16:36:22 -0500

    Update KaprekarDemo.java

--- src/unl/cse/kaprekar/KaprekarDemo.java
+++ src/unl/cse/kaprekar/KaprekarDemo.java
@@ -2,18 +2,18 @@ package unl.cse.kaprekar;
 
 public class KaprekarDemo {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		
 		if(args.length != 1) {
-			System.err.println("Usage: KaprekarDemo n");
+			System.out.println("Usage: KaprekarDemo n");
 			System.exit(1);
 		}
 		
 		int n = Integer.parseInt(args[0]);
 		if(KaprekarUtils.isKaprekar(n)) {
-			System.out.printf("%d is a Kaprekar Number!\n", n);
+			System.out.printf(n + " is a Kaprekar Number!\n");
 		} else {
-			System.out.printf("%d is not a Kaprekar Number!\n", n);
+			System.out.printf(n + " is not a Kaprekar Number!\n");
 		}		
 	}
 }
